7bbc991089e6571b22b9edfeb8a395c49065b131
Dong Hur <dhur@college.harvard.edu>
2019-07-30 16:00:56 -0400

    video and bpgraph now works

--- main.py
+++ main.py
@@ -1,25 +1,95 @@
 import sys
 import os
 import numpy as np
+from glob import glob
 
 # from PyQt5.QtGui import 
-# from PyQt5.QtCore import 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import (QDir)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QFileDialog)
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 
 from UI.Ui_MainWindow import Ui_MainWindow
+from tools.BP_Graph import BP_Canvas
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-	def __init__(self):
-		super(MainWindow, self).__init__()
-		self.setupUi(self)
-		self.show()
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.setup_video()
+        self.setup_connection()
+        self.show()
+    def setup_connection(self):
+        self.actionOpen.triggered.connect(self.open_clicked)
+        self.VideoSlider.sliderMoved.connect(self.slider_moved)
+        # self.updateButton.clicked.connect()
+        # self.filter1Button.clicked.connect()
+        # self.playButton.clicked.connect()
+        # self.singleLeftButton.clicked.connect()
+        # self.doubleLeftButton.clicked.connect()
+        # self.singleRightButton.clicked.connect()
+        # self.doubleRightButton.clicked.connect()
+
+        # self.VideoSlider.setValue(position)
+        pass
+    def setup_video(self):
+        self.BPCanvas = BP_Canvas()
+        self.videoGridLayout.addWidget(self.BPCanvas)
+        pass
+    def open_clicked(self):
+        video_filename = ''
+        DLC_filename = ''
+        directory = QFileDialog.getExistingDirectory(None, "Select Directory", QDir.homePath())
+        if directory != '' or directory != None:
+            filename_key = directory.split('/')[-1]
+
+            # find video filename
+            vid_file = glob(directory+"/"+filename_key+'.avi')
+            if len(vid_file) != 0:
+                video_dir = vid_file[0]
+            else:
+                print("::Could Not Find Video File: "+directory+"/"+filename_key+".avi")
+
+            # find DeepLabCut filename
+            DLC_file = glob(directory+"/"+filename_key+"*.h5")
+            if len(vid_file) != 0:
+                DLC_dir = DLC_file[0]
+            else:
+                print("::Could Not Find DeepLabCut File: "+directory+"/"+filename_key+"*.h5")
+
+            if video_dir!='' and DLC_dir!='':
+                self.BPCanvas.load_file(video_dir, DLC_dir)
+                # update slider
+                self.VideoSlider.setRange(0, self.BPCanvas.num_frame)
+            else:
+                print("::FAILED TO LOAD DATA")
+        pass
+    def slider_moved(self, frame):
+        self.BPCanvas.update_canvas(frame)
+        pass
 
 
 
 
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	mainWin = MainWindow()
-	ret = app.exec_()
-	sys.exit(ret)
+    app = QApplication(sys.argv)
+    mainWin = MainWindow()
+    ret = app.exec_()
+    sys.exit(ret)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tools/BP_Graph.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+import cv2
+
+from PyQt5.QtWidgets import (QWidget)
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+import matplotlib.image as mpimg
+
+class BP_Canvas(FigureCanvas):
+    def __init__(self, *args, **kwargs):
+        self.fig = Figure()
+        super(BP_Canvas, self).__init__(self.fig)
+        self.filepath = ''
+        self.data = None
+        self.ax = None
+        self.num_frame = 0
+        self.num_bp = 30
+        self.setup_canvas()
+    def setup_canvas(self):
+        self.ax = self.fig.add_subplot(111)
+        self.ax.plot([], 'o-')
+        self.ax.set_title('Body Point', fontsize=8)
+        self.ax.tick_params(axis='both', labelsize=6)
+        self.draw()
+        # connect on click graph
+        self.fig.canvas.mpl_connect('button_press_event', self.onClick)
+        pass
+    def load_file(self, video_dir, DLC_dir):
+        # setup ant video
+        self.cap = cv2.VideoCapture(video_dir)
+        # setup DeepLabCut
+        bp_h5data = pd.read_hdf(DLC_dir)
+        bp_data = bp_h5data[ bp_h5data.keys().levels[0][0] ].values # converts h5 to npy
+        self.num_frame = bp_data.shape[0]
+        bp_data = np.delete( bp_data.reshape( self.num_frame,self.num_bp,-1 ), obj=-1, axis=2 ) # reformats data and takes out last prob varaiable
+        self.data = np.swapaxes(bp_data.T,0,1) # num_bp x num_coord x t
+        # udpate video and bodypoint graph
+        self.update_canvas(frame=0)
+        # cap.release()
+        pass
+    def update_canvas(self, frame=0):
+        self.ax.clear()
+        # update ant video
+        self.cap.set(1, frame)
+        _, framePic = self.cap.read()
+        self.ax.imshow(framePic)
+        # update DeepLabCut
+        self.plot_bpgraph(frame)
+        self.draw()
+        pass
+    def plot_bpgraph(self, frame=0):
+        # self.fig.clear()
+        # ax = self.add_subplot(111)
+        marker = 'o'
+        s=4
+        # plot graph
+        self.ax.plot(self.data[0:4,0,frame], self.data[0:4,1,frame], marker=marker, markersize=s)
+        self.ax.plot(self.data[4:8,0,frame], self.data[4:8,1,frame], marker=marker, markersize=s)
+        self.ax.plot(self.data[8:11,0,frame], self.data[8:11,1,frame], marker=marker, markersize=s)
+        self.ax.plot(self.data[11:14,0,frame], self.data[11:14,1,frame], marker=marker, markersize=s)
+        self.ax.plot(self.data[14:17,0,frame], self.data[14:17,1,frame], marker=marker, markersize=s)
+        self.ax.plot(self.data[17:21,0,frame], self.data[17:21,1,frame], marker=marker, markersize=s)
+        self.ax.plot(self.data[21:24,0,frame], self.data[21:24,1,frame], marker=marker, markersize=s)
+        self.ax.plot(self.data[24:27,0,frame], self.data[24:27,1,frame], marker=marker, markersize=s)
+        self.ax.plot(self.data[27:30,0,frame], self.data[27:30,1,frame], marker=marker, markersize=s)
+        pass
+    def onClick(self, event):
+        print("x: ", event.xdata)
+        print("y: ", event.ydata)
+        pass
+
+
+
+class BP_Graph(Figure):
+    def __init__(self, *args, **kwargs):
+        super(BP_Graph, self).__init__()
+        self.bp_filepath = ''
+        self.num_frame = 0
+        self.duration = 1
+        self.data = None
+    def init_plot(self):
+        self.clear()
+        ax = self.add_subplot(111)
+        ax.plot([], 'o-')
+        ax.set_title('Ant Body Point Graph', fontsize=8);
+        ax.tick_params(axis='both', labelsize=6);
+        pass
+    def set_newfile(self, filepath):
+        self.bp_filepath = filepath
+        self.data = np.load(filepath) # (30,2,5901)
+        self.num_frame = self.data.shape[2]
+        print("BP_GRAPH Data: ", self.data.shape)
+        pass
+    def set_duration(self, duration):
+        self.duration = duration
+        pass
+    def update_graph(self, position, frame_data=False):
+        if frame_data:
+            frame = position # frame as data point
+        else:
+            frame = int(self.num_frame*position/self.duration)-1 # index is one less; millisecond as data
+        self.clear()
+        ax = self.add_subplot(111)
+        # plot ant points for specific time point t; specific to out setup with 30bp ants
+        # data format: num_bp x (X_coord, Y_coord) x t
+        # ax.scatter(self.data[:,0,frame], self.data[:,1,frame])
+        # graph parameters
+        marker = 'o'
+        s=4
+        # plot graph
+        ax.plot(self.data[0:4,0,frame], self.data[0:4,1,frame], marker=marker, markersize=s)
+        ax.plot(self.data[4:8,0,frame], self.data[4:8,1,frame], marker=marker, markersize=s)
+        ax.plot(self.data[8:11,0,frame], self.data[8:11,1,frame], marker=marker, markersize=s)
+        ax.plot(self.data[11:14,0,frame], self.data[11:14,1,frame], marker=marker, markersize=s)
+        ax.plot(self.data[14:17,0,frame], self.data[14:17,1,frame], marker=marker, markersize=s)
+        ax.plot(self.data[17:21,0,frame], self.data[17:21,1,frame], marker=marker, markersize=s)
+        ax.plot(self.data[21:24,0,frame], self.data[21:24,1,frame], marker=marker, markersize=s)
+        ax.plot(self.data[24:27,0,frame], self.data[24:27,1,frame], marker=marker, markersize=s)
+        ax.plot(self.data[27:30,0,frame], self.data[27:30,1,frame], marker=marker, markersize=s)
+        ax.set_xlim(left=-200, right=200)
+        ax.set_ylim(bottom=-200, top=200)
+        ax.set_aspect('equal', 'box')
+        ax.set_title('Ant Body Point Graph', fontsize=8);
+        ax.tick_params(axis='both', labelsize=6);
+        pass
+
+
+
+
+
+
+
