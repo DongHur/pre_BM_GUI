c12f5385380a4ca26390fc823aa3b5c3688a5d98
Dong Hur <dhur@college.harvard.edu>
2019-08-01 10:34:47 -0400

    added unfocus functionality when you click outside the textbox

--- UI/Ui_MainWindow.py
+++ UI/Ui_MainWindow.py
@@ -14,6 +14,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(689, 797)
+        MainWindow.setFocusPolicy(QtCore.Qt.ClickFocus)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.updateButton = QtWidgets.QPushButton(self.centralwidget)
